12423e64b2fe6391f1c07478b7cd8dda09d3d3d4
yasmanidominguez7-oss <yasmanidominguez7@gmail.com>
2026-08-21 08:18:52 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="aei-control-v2";const ASSETS=["./","./index.html","./manifest.json","./logo.jpg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{let q=r.clone();caches.open(CACHE).then(x=>x.put(e.request,q));return r}).catch(()=>caches.match("./index.html"))))});
